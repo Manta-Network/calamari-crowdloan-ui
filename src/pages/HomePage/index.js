d6749ac1e9cbffcc0bbfa75c3a7011340cc7f904
sinanbaya <sinanbaya@yahoo.com>
2021-07-10 13:08:19 -0700

    Add the home page content layout

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Navbar from 'components/Layouts/Navbar';
+import { Grid } from 'semantic-ui-react';
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <Navbar />
       <div className="home-content">
+      <Grid columns='three'>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="content-item contribute">
+              <h1 className="title">Contribute</h1>
+              <span>Enter Your Contribution Amount</span>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="content-item details">
+              <h1 className="title">Your Details</h1>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="content-item crowdloan">
+              <h1 className="title">The Crowdloan</h1>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
       </div>
     </div>
   );
